d5bc0b1bd82fae825acc9b91b5f3e6ff82d2c604
Gabriel Santos Rulo <55097727+GSRulo@users.noreply.github.com>
2021-04-09 21:40:38 -0300

    Comit de Micros Serviços.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+app.use(express.json());
+const palavraChave = "importante";
+
+const funcoes = {
+    observacaoCriada: (observacao) => {
+        observacao.status = observacao.texto.includes(palavraChave) ?
+            "importante" :
+            "comum";
+
+        axios.post("http://localhost:10000/eventos", {
+            tipo: "observacaoClassificada",
+            dados: observacao,
+        });
+    }
+}
+
+app.post("/eventos", (req, res) => {
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
+    res.status(200).send({
+        msg: "OK"
+    });
+});
+
+app.listen(7000, () => console.log("Classificação. Porta 7000"));
